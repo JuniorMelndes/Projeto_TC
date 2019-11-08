9ce80b97ca72b2c429ba6571217fefc287d301d4
JuniorMelndes <jose.adriao.junior@ccc.ufcg.edu.br>
2019-11-08 10:23:20 -0300

    correção de bugs

--- tc.py
+++ tc.py
@@ -31,28 +31,17 @@ def execucao (estados, estado_atual, aceita, transicoes, entrada):
 		caminhos_possiveis = 0
 		j = 0
 		i = 0 
-		#Este laço determina a quantidade de caminhos disponiveis que um automato pode seguir em um dado estado
-		while (i < len(transicoes)):
-			if((transicoes[i] == estado_atual) and (entrada[0] == transicoes[i+2])):
-				caminhos_possiveis += 1
-			i+= 3
-		#Este laço determina a quantidade de caminhos disponiveis que um automato pode seguir em um dado estado
-
-		#Sobre estes dois laços, o primeiro garante que todos os caminhos do estado sejam seguidos com informação adquirida no primeir			o e o segundo garante isso para todos os estados com um apoio recursivo
-		for k in range(caminhos_possiveis):
-			while(j < len(transicoes)): 
-				if((transicoes[j] == estado_atual) and (entrada[0] == transicoes[j+2])): 
-					entrada_reserva = []
-					for l in entrada:
-						entrada_reserva += l
-					del(entrada_reserva[0])
-					if(execucao (estados, transicoes[j+1], aceita, transicoes, entrada_reserva)):
-						passou = True
-				j+= 3
-			del(entrada[0])
-	
-		#Sobre estes dois laços, o primeiro garante que todos os caminhos do estado sejam seguidos com informação adquirida no primeir			o e o segundo garante isso para todos os estados com um apoio recursivo
-	if(estado_atual in aceita):
+		while(j < len(transicoes)): 
+			if((transicoes[j] == estado_atual) and (entrada[0] == transicoes[j+2])):
+				entrada_reserva = []
+				for l in entrada:
+					entrada_reserva += l
+				del(entrada_reserva[0])
+				if(execucao (estados, transicoes[j+1], aceita, transicoes, entrada_reserva)):
+					passou = True
+			j+= 3
+		break
+	if(entrada == [] and (estado_atual in aceita)):
 		passou = True
 	return passou
 #Execução do autômato
